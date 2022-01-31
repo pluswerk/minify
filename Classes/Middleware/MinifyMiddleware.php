3e73cf8e0aff3df016894a8716eff88262a06dc4
Stefan Lamm <s.lamm@andersundsehr.com>
2022-01-31 15:35:46 +0100

    [BUGFIX] makeInstance

--- Classes/Middleware/MinifyMiddleware.php
+++ Classes/Middleware/MinifyMiddleware.php
@@ -11,15 +11,10 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\Response;
 use TYPO3\CMS\Core\Http\Stream;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class MinifyMiddleware implements MiddlewareInterface
 {
-    protected MinifyService $minifyService;
-    public function __construct(MinifyService $minifyService)
-    {
-        $this->minifyService = $minifyService;
-    }
-
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         //  minimize only html
@@ -34,7 +29,9 @@ class MinifyMiddleware implements MiddlewareInterface
             $body = $response->getBody();
             $body->rewind();
             $html = $body->getContents();
-            $html = $this->minifyService->minify($html);
+
+            $minifyService = GeneralUtility::makeInstance(MinifyService::class);
+            $html = $minifyService->minify($html);
             $body = new Stream('php://temp', 'wb+');
             $body->write($html);
             $response = $response->withBody($body);
